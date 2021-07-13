03ddacefd9c75a36bb710c07fcec7b319474fb9e
trizzle21 <tdross21@gmail.com>
2021-07-13 17:18:56 -0400

    API-518/demo-app: black

--- apps/python/src/app.py
+++ apps/python/src/app.py
@@ -16,23 +16,25 @@ from dotenv import load_dotenv, find_dotenv
 from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv(find_dotenv())
-CLIENT_ID = os.getenv('CLIENT_ID')
-CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 REDIRECT_URI = os.getenv("REDIRECT_URI")
 
 # TODO: move to prod when we eventually make this public
-ATTENTIVE_API_URL = 'https://api-devel.attentivemobile.com/v1/'
-ACCESS_TOKEN_ENDPOINT = f'https://ui-api-devel.attentivemobile/authorization-codes/tokens'
+ATTENTIVE_API_URL = "https://api-devel.attentivemobile.com/v1/"
+ACCESS_TOKEN_ENDPOINT = (
+    f"https://ui-api-devel.attentivemobile/authorization-codes/tokens"
+)
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////code/data/demo.sqlite"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
 
 class Applications(db.Model):
-    __tablename__ = 'applications'
+    __tablename__ = "applications"
 
     id = db.Column(db.Integer, primary_key=True)
     token = db.Column(db.String())
@@ -41,20 +43,20 @@ class Applications(db.Model):
     created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return f'<application <id {self.id}>'
+        return f"<application <id {self.id}>"
 
 
 db.init_app(app)
 db.create_all()
 
 
-@app.route('/install')
+@app.route("/install")
 def redirect_to_install_page():
     # Generates a random string containing numbers and letters
-    state = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
+    state = "".join(random.choice(string.ascii_lowercase) for i in range(10))
 
     # List of scopes that your app needs
-    scopes = ['ecommerce:write', 'events:write', 'subscriptions:write']
+    scopes = ["ecommerce:write", "events:write", "subscriptions:write"]
 
     application = Applications(state=state)
     db.session.add(application)
@@ -62,45 +64,55 @@ def redirect_to_install_page():
 
     # TODO: move to prod and delete internal note when we eventually make this public
     # Note: to internal devs, feel free to use localhost:4000 when testing in dev
-    redirect_url = f'https://ui-devel.attentivemobile.com/integrations/oauth-install?client_id={CLIENT_ID}' \
-                   f'&redirect_uri={REDIRECT_URI}' \
-                   f'&scope={"+".join(scopes)}' \
-                   f'&state={state}'
+    redirect_url = (
+        f"https://ui-devel.attentivemobile.com/integrations/oauth-install?client_id={CLIENT_ID}"
+        f"&redirect_uri={REDIRECT_URI}"
+        f'&scope={"+".join(scopes)}'
+        f"&state={state}"
+    )
 
     # Your application requests authorization
     return redirect(redirect_url, code=302)
 
 
-@app.route('/callback')
+@app.route("/callback")
 def redirect_from_install_page():
-    authorization_code = request.args.get('authorizationCode')
-    state = request.args.get('state')
+    authorization_code = request.args.get("authorizationCode")
+    state = request.args.get("state")
 
     application: Applications = Applications.query.filter_by(state=state).first()
     if not application:
-        return 'no application'
+        return "no application"
 
     # your application makes a request to exchange an authorization code for an access_token
-    response = requests.post(ACCESS_TOKEN_ENDPOINT,
-                             json={'grantType': 'authorization_code',
-                                   'code': authorization_code,
-                                   'redirectUri': REDIRECT_URI,
-                                   'clientId': CLIENT_ID,
-                                   'clientSecret': CLIENT_SECRET})
+    response = requests.post(
+        ACCESS_TOKEN_ENDPOINT,
+        json={
+            "grantType": "authorization_code",
+            "code": authorization_code,
+            "redirectUri": REDIRECT_URI,
+            "clientId": CLIENT_ID,
+            "clientSecret": CLIENT_SECRET,
+        },
+    )
 
-    access_token = response.json().get('token')
+    access_token = response.json().get("token")
     if not access_token:
-        return 'no token'
+        return "no token"
 
     # make a request to /me endpoint to access Attentive API
-    me_response = requests.get(ATTENTIVE_API_URL + 'me',
-                               headers={'Content-Type': 'application/json',
-                                        'Authorization': f'Bearer {access_token}'})
-    company_name = me_response.json().get('companyName')
+    me_response = requests.get(
+        ATTENTIVE_API_URL + "me",
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {access_token}",
+        },
+    )
+    company_name = me_response.json().get("companyName")
 
     # store access token and information about the installing company if necessary
     application.token = access_token
     application.name = company_name
     db.session.add(application)
     db.session.commit()
-    return 'Company Successfully Installed'
+    return "Company Successfully Installed"
