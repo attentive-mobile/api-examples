3ec14e8d9e6c628f525347f5758049325d9159fd
trizzle21 <tdross21@gmail.com>
2021-11-29 11:43:41 -0500

    AP-647/public-node-app: added example env, added new endpoints and new package.json

--- apps/node/server.js
+++ apps/node/server.js
@@ -1,11 +1,107 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const axios = require('axios');
+const dotenv = require('dotenv').config()
+const crypto = require('crypto');
+
+let app = express()
+app.use(express.json())
+
 const port = 3002
 
+const ATTENTIVE_API_URL = "https://api-devel.attentivemobile.com/v1/";
+const ACCESS_TOKEN_ENDPOINT = "https://api-devel.attentivemobile.com/v1/authorization-codes/tokens"
+
+var CLIENT_ID = dotenv.parsed.CLIENT_ID; // Your client id
+var CLIENT_SECRET = dotenv.parsed.CLIENT_SECRET; // Your secret
+var REDIRECT_URI = dotenv.parsed.REDIRECT_URI; // Your redirect uri
+
 app.get('/', (req, res) => {
-  res.send({"message": "Your Application is running successfully!"})
+  res.send({"message": "Your Application is running successfully!"});
+});
+
+app.get('/install', async (req, res) => {
+  // Generates a random string containing numbers and letters
+  const state = generateRandomString(10)
+
+  // List of scopes that your app needs
+  const scopes = ["ecommerce:write", "events:write", "subscriptions:write"]
+
+  const redirect_url =
+    `https://ui-devel.attentivemobile.com/integrations/oauth-install?client_id=${CLIENT_ID}` + 
+    `&redirect_uri=${REDIRECT_URI}` +
+    `&scope=${scopes.join("+")}` +
+    `&state=${state}`;
+
+  res.redirect(redirect_url);
+});
+  
+app.get('/callback', async (req, res) => {
+  const authorization_code = req.query.authorizationCode;
+  const state = req.query.state;
+
+// TODO: add application to in memory db
+
+  const payload = JSON.stringify({
+    "grant_type": "authorization_code",
+    "code": authorization_code,
+    "redirect_uri": REDIRECT_URI,
+    "client_id": CLIENT_ID,
+    "client_secret": CLIENT_SECRET,
+  });
+  // your application makes a request to exchange an authorization code for an access_token
+  const response = await axios.post(ACCESS_TOKEN_ENDPOINT, {
+    data: JSON.stringify(payload), 
+    headers: {'Content-Type': 'application/json; charset=utf-8'}
+  });
+  const access_token = response.data.access_token;
+  const me_response = await axios.get(`${ATTENTIVE_API_URL}/me`, {
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${access_token}`,
+    }
+  });
+  company_name = me_response.data.companyName;
+
+  // build redirect url and return 302
+  res.send({"message": "Your Application is installed successfully!"});
+});
+
+
+app.post('/webhooks', async (req, res) => {
+  const webhook_secret = dotenv.parsed.WEBHOOK_SECRET;
+  const payload  = JSON.stringify(req.body);
+  if (webhook_secret) {
+    const signature = req.headers['x-attentive-hmac-sha256'];
+
+    const hmac = crypto.createHmac('sha256', webhook_secret);
+    const digest = Buffer.from(hmac.update(payload).digest('hex'), 'utf8');
+    const checksum = Buffer.from(signature, 'utf8');
+
+    if (checksum.length !== digest.length || !crypto.timingSafeEqual(digest, checksum)) {
+      console.log(
+        `‼️ Webhook received with invalid signature! digest: ${digest}, checksum: ${checksum}, ` +
+        `request body: ${payload}`);
+      res.status(403).json({status:"invalid signature"});
+      return;
+    }
+  }
+
+  const event_type = req.body.type;
+  console.log(`🔔 Webhook of type ${event_type} received! ${payload}`);
+  res.status(200).json({"status": "success"});
 });
 
 app.listen(port, () => {
-  console.log(`Example app 1 listening at http://localhost:${port}`)
+  console.log(`Demo app is listening at http://localhost:${port}`)
 })
+
+
+const generateRandomString = (length) => {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
